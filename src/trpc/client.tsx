3e9107adfd99b010fe1a3534317b81b275793186
Samson <126519078+korisnik-samson@users.noreply.github.com>
2025-12-06 20:48:08 +0100

    Modified - SuperJson Enabled for TRPC Integration

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,10 +1,11 @@
 'use client';
 // ^-- to make sure we can mount the Provider from a server component
+import superjson from 'superjson';
 import type { QueryClient } from '@tanstack/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import { createTRPCContext } from '@trpc/tanstack-react-query';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
@@ -41,7 +42,7 @@ export function TRPCReactProvider(props: Readonly<{ children: React.ReactNode; }
         createTRPCClient<AppRouter>({
             links: [
                 httpBatchLink({
-                    // transformer: superjson, <-- if you use a data transformer
+                    transformer: superjson, // <-- if you use a data transformer
                     url: getUrl(),
                 }),
             ],
