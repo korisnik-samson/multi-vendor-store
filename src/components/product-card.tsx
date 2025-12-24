8f0a3105a20c22f098e8fc634ab99700975b2ad2
Samson <126519078+korisnik-samson@users.noreply.github.com>
2025-12-24 12:39:44 +0100

    Update product-card.tsx

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -57,6 +57,7 @@ export const ProductCard = ({ id, name, imageUrl, tenantImageUrl, tenantSubDomai
     )
 };
 
+// @ts-ignore
 export const ProductCardSkeleton = () => {
     return <div className='w-full aspect-3/4 bg-neutral-200 rounded-lg animate-pulse' />
 }
